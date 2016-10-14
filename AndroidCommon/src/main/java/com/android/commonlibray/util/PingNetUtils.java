72854d77f360fcee3ba6d8d05d890b2be3d2a1b9
leon2017 <lijiawangjun@gmail.com>
2016-10-14 15:29:12 +0800

    添加百度 ping网络连接状态

---
+++ AndroidCommon/src/main/java/com/android/commonlibray/util/PingNetUtils.java
@@ -0,0 +1,51 @@
+package com.android.commonlibray.util;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * 当前类注释：根据ping百度来判断网络状态，这个是最直接判断有没有网络的方法
+ * Author :LeonWang
+ * Created  2016/10/14.15:25
+ * Description:
+ * E-mail:lijiawangjun@gmail.com
+ */
+
+public class PingNetUtils {
+
+    public static boolean getNetPing() {
+        String result = null;
+        try {
+            String ip = "www.baidu.com";// 除非百度挂了，否则用这个应该没问题~
+            Process p = Runtime.getRuntime().exec("ping -c 1 -w 100 " + ip);// ping1次
+            // 读取ping的内容，可不加。
+            InputStream input = p.getInputStream();
+            BufferedReader in = new BufferedReader(new InputStreamReader(input));
+            StringBuffer stringBuffer = new StringBuffer();
+            String content = "";
+            while ((content = in.readLine()) != null) {
+                stringBuffer.append(content);
+            }
+            Log.i("TTT", "result content : " + stringBuffer.toString());
+            // PING的状态
+            int status = p.waitFor();
+            if (status == 0) {
+                result = "successful~";
+                return true;
+            } else {
+                result = "failed~ cannot reach the IP address";
+            }
+        } catch (IOException e) {
+            result = "failed~ IOException";
+        } catch (InterruptedException e) {
+            result = "failed~ InterruptedException";
+        } finally {
+            Log.i("TTT", "result = " + result);
+        }
+        return false;
+    }
+}
