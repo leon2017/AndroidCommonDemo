644b4907f03cb12e50a95db39d3e10b36c863d81
leon2017 <lijiawangjun@gmail.com>
2016-10-18 17:02:32 +0800

    添加发送验证码倒计时

---
+++ AndroidCommon/src/main/java/com/android/commonlibray/util/CountDownTimeUtils.java
@@ -0,0 +1,66 @@
+package com.android.commonlibray.util;
+
+import android.graphics.Color;
+import android.os.CountDownTimer;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
+
+/**
+ * 当前类注释：倒计时工具类
+ * Author :LeonWang
+ * Created  2016/10/18.16:43
+ * Description:使用
+ * CountDownTimeUtils countDownTimeUtils = new CountDownTimeUtils(mTvGetCheckCode, 60000, 1000);
+ * countDownTimeUtils.start();
+ * E-mail:lijiawangjun@gmail.com
+ */
+
+public class CountDownTimeUtils extends CountDownTimer {
+
+    private TextView mTextView;
+    /**
+     * @param millisInFuture    The number of millis in the future from the call
+     *                          to {@link #start()} until the countdown is done and {@link #onFinish()}
+     *                          is called.
+     * @param countDownInterval The interval along the way to receive
+     *                          {@link #onTick(long)} callbacks.
+     */
+    public CountDownTimeUtils(TextView textView, long millisInFuture, long countDownInterval) {
+        super(millisInFuture, countDownInterval);
+        this.mTextView = textView;
+    }
+
+    @Override
+    public void onTick(long millisUntilFinished) {
+        mTextView.setClickable(false);//设置不可点击
+        mTextView.setText(millisUntilFinished/1000+"秒后可重新发送");
+
+        /**
+         * 超链接 URLSpan
+         * 文字背景颜色 BackgroundColorSpan
+         * 文字颜色 ForegroundColorSpan
+         * 字体大小 AbsoluteSizeSpan
+         * 粗体、斜体 StyleSpan
+         * 删除线 StrikethroughSpan
+         * 下划线 UnderlineSpan
+         * 图片 ImageSpan
+         */
+        SpannableString spannableString = new SpannableString(mTextView.getText().toString());  //获取按钮上的文字
+        ForegroundColorSpan span = new ForegroundColorSpan(Color.parseColor("#ff24a9f6"));
+        /**
+         * public void setSpan(Object what, int start, int end, int flags) {
+         * 主要是start跟end，start是起始位置,无论中英文，都算一个。
+         * 从0开始计算起。end是结束位置，所以处理的文字，包含开始位置，但不包含结束位置。
+         */
+        spannableString.setSpan(span, 0, 2, Spannable.SPAN_INCLUSIVE_EXCLUSIVE);//将倒计时的时间设置为红色
+        mTextView.setText(spannableString);
+    }
+
+    @Override
+    public void onFinish() {
+        mTextView.setText("重新获取验证码");
+        mTextView.setClickable(true);//重新获得点击
+    }
+}
